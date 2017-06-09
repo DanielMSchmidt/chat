ae7a3b6bc8a0f0ce15160f71654dea40b403dae3
David E <davide135@gmail.com>
2017-06-09 23:15:28 +0200

    add basic app screens

--- mobile/App.js
+++ mobile/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from "./LoginScreen";
+import ChatScreen from "./ChatScreen";
 
 export default class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {username: ''};
+    }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+        if(this.state.username === ''){
+            return (
+                <LoginScreen onPress={(text)=> this.setState({username: text})}/>
+            );
+        }
+        return (
+            <ChatScreen onPress={(text) => console.log(text)}/>
+        )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/LoginScreen.js
@@ -0,0 +1,43 @@
+/**
+ * Created by david on 09.06.17.
+ */
+import React from 'react';
+import {Alert, Button, StyleSheet, Text, TextInput, View} from 'react-native';
+
+export default class LoginScreen extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {text: ''};
+        this.onPress = this.onPress.bind(this);
+    }
+
+    onPress(){
+        this.props.onPress(this.state.text)
+    }
+
+    render() {
+
+        return (
+            <View style={styles.container}>
+                <Text> Start chatting...</Text>
+                <TextInput
+                    style={{height: 40, width: 200}}
+                    placeholder="Username"
+                    onChangeText={(text) => this.setState({text})}
+                />
+                <Button title="Start"
+                        onPress={this.onPress} />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
